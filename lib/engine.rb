f081f34a9f995eeb3671050cbf99469d29a58ad1
Mateo Murphy <mateo@mateo-murphys-macbook-pro-4.local>
2011-02-09 16:06:05 -0500

    Small cleanup

--- lib/engine.rb
+++ lib/engine.rb
@@ -5,8 +5,8 @@ module AppFrame
     end
     
     initializer "rails includes" do |app|
-      ActionController::Base.send(:include, AppFrame::ControllerMethods)
-      ActionView::Base.send(:include, AppFrame::ViewMethods)
+      ActionController::Base.send :include, AppFrame::ControllerMethods
+      ActionView::Base.send       :include, AppFrame::ViewMethods
     end
     
   end
