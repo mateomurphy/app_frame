f124ad83ba968232c47faa64b80c6ffcf15fe625
Mateo Murphy <mateo.murphy@gmail.com>
2011-10-31 21:26:49 -0400

    Fix class generated by menu helper

--- app/helpers/app_frame/menu_helper.rb
+++ app/helpers/app_frame/menu_helper.rb
@@ -2,9 +2,9 @@ module AppFrame
   module MenuHelper
     def menu_link(key, path, options = {})
       if options[:highlights_on]
-        options[:class] = 'select' if request.path =~ options.delete(:highlights_on)
+        options[:class] = 'active' if request.path =~ options.delete(:highlights_on)
       else
-        options[:class] = 'select' if request.path =~ /#{path}/
+        options[:class] = 'active' if request.path =~ /#{path}/
       end
     
       content_tag :li, link_to(key, path), options
