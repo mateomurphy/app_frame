710c4b3bc61d31bffee83a7d1bb23efb694333fd
Mateo Murphy <mateo.murphy@gmail.com>
2011-11-07 19:43:35 -0500

    Updated menu code

--- app/helpers/app_frame/menu_helper.rb
+++ app/helpers/app_frame/menu_helper.rb
@@ -1,13 +1,19 @@
 module AppFrame
   module MenuHelper
-    def menu_link(key, path, options = {})
+    def menu_link(key, path, options = {}, &block)
       if options[:highlights_on]
         options[:class] = 'active' if request.path =~ options.delete(:highlights_on)
       else
         options[:class] = 'active' if request.path =~ /#{path}/
       end
     
-      content_tag :li, link_to(key, path), options
+      key = t(:"menu.#{key}") if key.is_a?(Symbol)
+    
+      content = link_to(key, path)
+    
+      content += content_tag(:ul, capture(&block)) if block_given?
+    
+      content_tag :li, content, options
     end  
   end
 end
