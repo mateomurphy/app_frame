dad51af16a41367a0ae24781f4ee1ff36e3a60a1
Mateo Murphy <mateo.murphy@gmail.com>
2012-12-29 20:27:23 -0500

    Show pagination result with a p tag

--- app/helpers/app_frame/pagination_helper.rb
+++ app/helpers/app_frame/pagination_helper.rb
@@ -4,9 +4,9 @@ module AppFrame::PaginationHelper
     return nil if count == 0
     
     range_start = (page - 1) * per_page + 1
-    range_end = (page)* per_page
+    range_end = (page) * per_page
     range_end = count if range_end > count
     
-    "<div class='page-range'>Displaying <strong>#{range_start} - #{range_end}</strong> of <strong>#{count}</strong> in total</div>".html_safe
+    "<p class='page-range'>Displaying <strong>#{range_start} - #{range_end}</strong> of <strong>#{count}</strong> in total</p>".html_safe
   end
 end
