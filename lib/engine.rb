8157c61ab73dd5abad3b69a24dc2e283562d4ec0
Mateo Murphy <mateo.murphy@gmail.com>
2013-11-04 15:22:42 -0500

    Add nested wrapper to bootstrap config

--- lib/engine.rb
+++ lib/engine.rb
@@ -97,6 +97,16 @@ module AppFrame
           end
         end
 
+        config.wrappers :nested, :tag => 'div', :class => 'control-group', :error_class => 'error' do |b|
+          b.use :html5
+          b.use :placeholder
+          b.wrapper :tag => 'div', :class => 'controls' do |ba|
+            ba.use :label_input
+            ba.use :error, :wrap_with => { :tag => 'span', :class => 'help-inline' }
+            ba.use :hint,  :wrap_with => { :tag => 'p', :class => 'help-block' }
+          end
+        end
+
         # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
         # Check the Bootstrap docs (http://twitter.github.com/bootstrap)
         # to learn about the different styles for forms and inputs,
